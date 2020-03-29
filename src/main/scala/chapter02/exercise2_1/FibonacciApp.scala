8d93bddf825bdf96b570cc85f1120a0d34dfeebb
Oto Brglez <otobrglez@gmail.com>
2020-03-29 23:07:32 +0200

    Update FibonacciApp.scala

--- src/main/scala/chapter02/exercise2_1/FibonacciApp.scala
+++ src/main/scala/chapter02/exercise2_1/FibonacciApp.scala
@@ -11,15 +11,6 @@ object Factorial {
   }
 }
 
-/**
- * static int fib(int n)
- * {
- * if (n <= 1)
- * return n;
- * return fib(n-1) + fib(n-2);
- * }
- */
-
 object Fibonacci {
   def fib(n: Int): Int = {
     def go(n: Int): Int =
